4b8b25899636baf5ebb7970071a89745a3b054fd
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-08-21 13:43:04 +0100

    Implement Creating Account Reference
    Implemented the creating of the account reference in the payments
    database

--- src/SFA.DAS.EAS.PaymentProvider.Worker/Providers/PaymentAccountCreated.cs
+++ src/SFA.DAS.EAS.PaymentProvider.Worker/Providers/PaymentAccountCreated.cs
@@ -1,6 +1,7 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using MediatR;
 using SFA.DAS.EmployerAccounts.Events.Messages;
+using SFA.DAS.EmployerPayments.Application.Commands.CreateAccountReference;
 using SFA.DAS.EmployerPayments.Domain.Attributes;
 using SFA.DAS.Messaging;
 using SFA.DAS.NLog.Logger;
@@ -10,17 +11,23 @@ namespace SFA.DAS.EmployerPayments.Worker.Providers
     
     public class PaymentAccountCreated : MessageProcessor<AccountCreatedMessage>
     {
+        private readonly ILog _log;
+        private readonly IMediator _mediator;
+
         [QueueName("employer_shared")]
         public string account_created_message { get; set; }
 
-        public PaymentAccountCreated(IPollingMessageReceiver pollingMessageReceiver, ILog log) : base(pollingMessageReceiver, log)
+        public PaymentAccountCreated(IPollingMessageReceiver pollingMessageReceiver, ILog log, IMediator mediator) : base(pollingMessageReceiver, log)
         {
-
+            _log = log;
+            _mediator = mediator;
         }
 
         protected override async Task ProcessMessage(AccountCreatedMessage messageContent)
         {
-            
+            _log.Info($"Processing adding account reference for AccountId:{messageContent.AccountId}");
+
+            await _mediator.SendAsync(new CreateAccountReferenceCommand {AccountId = messageContent.AccountId});
         }
     }
 }
